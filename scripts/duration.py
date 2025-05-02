51e107545a79cd1e4a8f2e3fc55ddba2c594b8a6
UgwayLegenda <yaroslavgovsha133@gmail.com>
2025-05-02 23:43:27 +0300

    Add files via upload

--- scripts/duration.py
+++ scripts/duration.py
@@ -1,1 +1,29 @@
+import pandas as pd
+import json
+import spotipy
+from spotipy import SpotifyClientCredentials
+from youtube_search import YoutubeSearch
 
+
+with open(".venv/config.json") as f:
+    config = json.load(f)
+
+lyrics = pd.read_csv('C:\Coding\Jazz analysis\lyrics_all.csv')
+def get_youtube_duration(artist, title):
+    try:
+        results = YoutubeSearch(f"{artist} {title}", max_results=1).to_dict()
+        return results[0]["duration"]
+    except:
+        return None
+
+lyrics["duration"] = lyrics.apply(lambda row: get_youtube_duration(row["artist"], row["title"]), axis=1)
+def convert_duration(duration):
+    minutes = int(duration)
+    seconds = round((duration - minutes) * 100)  # Извлекаем две цифры после точки
+    return (minutes * 60)  + seconds
+
+# Применяем функцию к колонке duration
+lyrics["duration"] = lyrics["duration"].apply(convert_duration)
+
+# Сохраняем результат в новый файл
+lyrics.to_csv("lyr_dur1_sec.csv", index=False)
